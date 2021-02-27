952d66cf70816c73e42333d06cb1a993804b90b3
Alexander Gnatenko <agnatenko@gmail.com>
2021-02-27 16:05:37 +0300

    Редактирование README.md

--- src/main/java/ru/idcore/Main.java
+++ src/main/java/ru/idcore/Main.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
